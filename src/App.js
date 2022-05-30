e4394a1f867a5fcaee639916fced14d0b00b5542
Andrey <artist_hire@ukr.net>
2022-05-30 11:23:19 +0300

    Test low case folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Profile from './components/Profile';
+import Profile from './components/profile';
 import Statistics from './components/Statistics';
 import FriendList from './components/FriendList';
 import TransactionHistory from './components/TransactionHistory';
