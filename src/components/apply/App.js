d249118594d5b7c59806a6aebfa58ef9bf51ecfc
Andrey <artist_hire@ukr.net>
2022-05-30 11:39:12 +0300

    Rename folder app

---
+++ src/components/apply/App.js
@@ -0,0 +1,33 @@
+import Profile from '../profile';
+import Statistics from '../statistics';
+import FriendList from '../friendlist';
+import TransactionHistory from '../transactionhistory';
+// data
+import userData from '../../data/user.json';
+import statistics from '../../data/data.json';
+import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
+// style
+import s from './App.module.scss';
+
+const App = () => {
+  return (
+    <div className={s.app}>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+
+      <Statistics title="Upload stats" stats={statistics} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
+
+export default App;
